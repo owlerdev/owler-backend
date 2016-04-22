31ecadb3d894374319ba3d8a97be3a73032c42dd
Will Theuer <watheuer@email.wm.edu>
2016-04-22 14:52:44 -0400

    Delete other user's vote test

--- spec/controllers/vote_controller_spec.rb
+++ spec/controllers/vote_controller_spec.rb
@@ -2,19 +2,24 @@ require 'rails_helper'
 
 RSpec.describe VoteController, type: :controller do
   before(:each) do
+    # create an event in the past
     @event = create(:event)
+
+    # create an event in the future
     @future_event = create(:future_event)
     sign_in @event.user
   end
 
   describe "POST vote#create" do
     it "upvotes an event that has started" do
+      # upvote the event in the past
       post :create, event: @event.attributes
       expect(response).to have_http_status(:success)
       expect(@event.upvotes).to eq(1)
     end
 
     it "adds hype to an event in the future" do
+      # add hype to event in the future
       post :create, event: @future_event.attributes
       expect(response).to have_http_status(:success)
       expect(@future_event.hype).to eq(1)
@@ -28,10 +33,26 @@ RSpec.describe VoteController, type: :controller do
     end
 
     it "deletes a user's upvote" do
+      # delete the signed in user's upvote
       delete :destroy,  id: @vote.id
       expect(response).to have_http_status(:success)
-      binding.pry
       expect(Vote.exists?(@vote.id)).to be false
+    end
+
+    it "can't delete another user's upvote" do
+      # future_event is by another user
+      sign_out @event.user
+      sign_in @future_event.user
+      post :create, event: @future_event.attributes
+      expect(response).to have_http_status(:success)
+      vote = JSON.parse(response.body)
+      sign_out @future_event.user
+
+      # sign in another user, try to delete other user's upvote
+      sign_in @event.user
+      binding.pry
+      delete :destroy, id: vote["id"]
+      expect(response).to have_http_status(:error)
     end
   end
 end
